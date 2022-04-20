a0feb4ef6fa0d0c1800aef958be486bcee61dccc
jordansmrd <jordansumardi@hotmail.com>
2022-04-20 18:19:00 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var name = 'jordan'
+// console.log(name);
+// var _name = "jordan";
+// var $name = "jordan";
+// var Name = "jordan";
+
+//rules var
+//1. var ga bisa spasi
+//2. rules letters, underscore, dollar sign
+
+
+
+//typeof function menentukan type data 
+var number1 = 8 //string
+var number2 = -5 //number
+var number3 = 13 //number
+
+
+//math object
+
+// console.log(Math.pow(number1,2))
+// console.log(Math.abs(number2))
+// console.log(Math.sqrt(number1))
+// console.log(Math.cbrt(number1))
+
+var random = Math.random()
+
+var stringnumber = '123'
+var num = 123
+// console.log(stringnumber) 
+// console.log(parseInt(stringnumber))// string diubah ke int
+// console.log(num.toString()) // int diubah ke string
+
+
+// \n  
+// console.log("hello \n there")
+// console.log(" i'll\tbe\tthere")
+// console.log('test angka ' + stringnumber)
+
+// console.log(`test angka ${stringnumber}`)
+
+
+// var x = "hello world "
+// console.log(x.length)
+// console.log(x.indexOf("world"))
+// console.log(x.substr(5,3))
+// console.log(x.substring(5,3))
+// console.log(x.slice(5,8))
+
+// console.log(x.toUpperCase())
+// console.log(x.toLowerCase())
+// console.log(x.split(' '))
+
+var x = 1
+if(x--)
+{
+    console.log('true')
+}
